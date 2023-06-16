18ebfcfaa65be43cdf2f2ba965dfa3aedcaf11ec
Luke Rogerson <luke@orbs.com>
2023-06-16 15:30:45 +0100

    Create Route53 hosted zone

--- lib/orbs-infra-stack.ts
+++ lib/orbs-infra-stack.ts
@@ -1,16 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_route53, aws_certificatemanager } from "aws-cdk-lib";
 
 export class OrbsInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OrbsInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new aws_route53.HostedZone(this, "LukeHostedZone", {
+      zoneName: "luke.com",
+    });
   }
 }
