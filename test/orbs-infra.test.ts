d94e0434a634037c71aef796f2741fc98b9ae6c9
Luke Rogerson <luke@orbs.com>
2023-06-16 15:31:02 +0100

    Add test to check for hosted zone

--- test/orbs-infra.test.ts
+++ test/orbs-infra.test.ts
@@ -1,17 +1,19 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as OrbsInfra from '../lib/orbs-infra-stack';
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import * as OrbsInfra from "../lib/orbs-infra-stack";
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/orbs-infra-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new OrbsInfra.OrbsInfraStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+describe("Domain management", () => {
+  test("TON hosted zone", () => {
+    const app = new cdk.App();
+    const stack = new OrbsInfra.OrbsInfraStack(app, "MyTestStack");
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+    const template = Template.fromStack(stack);
+    console.log("template :", template);
+
+    template.hasResourceProperties("AWS::Route53::HostedZone", {
+      Name: "luke.com.",
+    });
+  });
 });
